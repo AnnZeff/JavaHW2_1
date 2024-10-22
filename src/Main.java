05010348166eb071bc612e2a2085ddf68e6dcb2d
Anastasia Zef <zeff1983@gmail.com>
2024-10-22 21:28:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int card = 40762; // стоимость билета
+        int bonus = card/20; // количество бонусных миль за каждые 20 рублей стоимости билета
+
+
+        System.out.println((bonus) + " начислено бонусных миль");
+    }
+}
